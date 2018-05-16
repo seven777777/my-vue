9732c0f0f7e2a47bf9f6afe5b6cb0452da4d4695
seven <zhangmin145440@ehousechina.com>
2018-05-16 15:55:02 +0800

    reset

--- src/main.js
+++ src/main.js
@@ -2,16 +2,13 @@
  * @Author: seven.zhang 
  * @Date: 2018-05-16 10:55:41 
  * @Last Modified by: seven.zhang
- * @Last Modified time: 2018-05-16 13:53:18
+ * @Last Modified time: 2018-05-16 15:52:19
  */
 'use strict';
 import Vue from 'vue'
 import App from './App'
-import VvUI from 'vvui' // 引入组件库
-import '../node_modules/VVUI/packages/theme-default/lib/index.css' // 引入样式库
 
 Vue.config.productionTip = false
-Vue.use(VvUI)
 
 new Vue({
   el: '#app',
